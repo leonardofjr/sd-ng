25c4fa7bcbd859a9ca5e9edb2db1609a7a514297
Leo <leonardo.f.jr@gmail.com>
2018-12-18 21:36:15 -0500

    Fixed Mobile Glitch

--- src/scripts.js
+++ src/scripts.js
@@ -78,9 +78,14 @@ $(document).ready(function () {
 
 
       
-   
+    /* This script is to collapse the bootstrap mobile menu when a nav item is clicked */
+    /* When a nav item is clicked */
+    $('.dropdown-item , .nav-item:not(.dropdown)').click(function (e) {
+        $('.nav-link').removeClass('active');
+        /* we will remove the bootstrap show class */
+        $('#navbarSupportedContent').removeClass('show');
+    }) 
 });
-
 
 
 
